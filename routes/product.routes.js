d8b73cbba15fd052d7be652fc1b40b8df80264e9
Saurav Kumar <vermasourav120@gmail.com>
2026-02-08 16:46:52 +0530

    sec final

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Product from "../models/Product.js";
+import Product from "../models/product.js";
 import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
